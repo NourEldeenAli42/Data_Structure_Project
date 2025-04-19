5d75ef6a9ec57ec35ebde5e8c9e051ae5da82300
Nour Eldeen Ali <cds.noureldinali250403@alexu.edu.eg>
2025-04-19 08:17:54 +0200

    Mahdy After Changes with Nour

--- src/Main.java
+++ src/Main.java
@@ -6,50 +6,10 @@ public class Main{
         Scanner input = new Scanner(System.in);
 
         Course_List Courses = new Course_List();
-        /*Courses.addCourse(77887,"Calculus");
-        Courses.addCourse(77890,"Linear Algebra");
-        Courses.addCourse(7791,"Data Structures");*/
 
         Student_List Students = new Student_List();
-        // hello world! 
-        //you are the bomba
-        /*Students.addStudent(2401244543L,"Nour Eldeen Ali",19,3.9);
-        Students.binarySearch (2401244543L).enroll (77887,Courses);
-        Students.binarySearch (2401244543L).enroll (77890,Courses);
-        Students.binarySearch (2401244543L).enroll (7791,Courses);
 
-        Students.addStudent(2401248710L,"Mahdy",19,4.0);
-        Students.binarySearch (2401248710L).enroll (77887,Courses);
-        Students.binarySearch (2401248710L).enroll (77890,Courses);
-        Students.binarySearch (2401248710L).enroll (7791,Courses);
 
-        Students.addStudent(2401247684L,"Mr. X",19,4.1);
-        Students.binarySearch (2401247684L).enroll (77887,Courses);
-        Students.binarySearch (2401247684L).enroll (77890,Courses);
-        Students.binarySearch (2401247684L).enroll (7791,Courses);
-
-        Students.addStudent(2401248195L,"Maro",19,4.2);
-        Students.binarySearch (2401248195L).enroll (77887,Courses);
-        Students.binarySearch (2401248195L).enroll (77890,Courses);
-        Students.binarySearch (2401248195L).enroll (7791,Courses);
-
-        Students.addStudent(2401247521L,"Mo'men",19,4.3);
-        Students.binarySearch (2401247521L).enroll (77887,Courses);
-        Students.binarySearch (2401247521L).enroll (77890,Courses);
-        Students.binarySearch (2401247521L).enroll (7791,Courses);
-
-        Students.addStudent(2402402402L,"Mo Waleed",12,5.0);
-        Students.binarySearch (2402402402L).enroll (77890,Courses);
-
-        Students.display ();
-
-        Students.MergeSort ();
-
-        Students.display ();
-
-        Students.binarySearch(2401244543L).display_courses();
-        Courses.binarySearch(7791).display_students();
-        Courses.binarySearch(77890).display_students();*/
         int Choice;
         long StudentID ;
         int CourseID;
@@ -88,9 +48,10 @@ public class Main{
 
                     if(Students.isExist(StudentID)){
 
-                        System.out.println("/nStudent is already exist.");
+                        System.out.println("\nStudent is already exist.");
                     }
                     else{
+                        input.nextLine();
 
                         System.out.println("Enter Student's Name");
                         StudentName = input.nextLine();
@@ -116,7 +77,7 @@ public class Main{
                     System.out.println("/nCourse is already exist.");
                 }
                 else{
-
+                    input.nextLine();
                     System.out.println("Enter Course's Name");
                     CourseName = input.nextLine();
     
@@ -131,7 +92,7 @@ public class Main{
                     
                     break;
                 case 4:
-                    
+
                     break;
                 case 5:
                     
